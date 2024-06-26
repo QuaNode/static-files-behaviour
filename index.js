deda94c2b2aa7c4fe02f80805aabeb9f88289e6e
Ahmed Ragab <ahmed.ragab@me.com>
2024-06-26 18:45:05 +0300

    v1.3.0

--- index.js
+++ index.js
@@ -13,35 +13,69 @@ module.exports = function (options) {
 
   if (!options) throw new Error('Invalid options');
   if (!Array.isArray(options)) options = [options];
-  return options.reduce(function (behaviours, behaviour_options) {
+  return options.reduce(function () {
 
-    if (typeof behaviour_options !== 'object')
+    var [behaviours, behaviour_options] = arguments;
+    if (typeof behaviour_options !== 'object') {
+
       throw new Error('Invalid options');
-    if (typeof behaviour_options.name !== 'string' ||
-      behaviour_options.name.length === 0)
-      throw new Error('Invalid behaviour name');
-    if (typeof behaviour_options.directory !== 'string' ||
-      behaviour_options.directory.length === 0)
-      throw new Error('Invalid resources directory');
-    behaviours[behaviour_options.name] = behaviour({
+    }
+    var {
+      name: behaviour_name,
+      version,
+      host,
+      path: behaviour_path,
+      queue,
+      storage,
+      directory,
+      index: default_index,
+      error: mapError
+    } = behaviour_options;
+    var named = typeof behaviour_name === 'string';
+    if (named) {
 
-      name: behaviour_options.name,
-      version: behaviour_options.version || '1',
-      path: behaviour_options.path || '/*',
-      host: behaviour_options.host,
+      named &= behaviour_name.length > 0;
+    }
+    if (!named) {
+
+      throw new Error('Invalid behaviour name');
+    }
+    var location = typeof directory === 'string';
+    if (location) {
+
+      location &= directory.length > 0;
+    }
+    if (!location) {
+
+      throw new Error('Invalid resources directory');
+    }
+    if (typeof mapError !== 'function') {
+
+      mapError = undefined;
+    }
+    behaviours[behaviour_name] = behaviour({
+
+      name: behaviour_name,
+      version: version || '1',
+      path: behaviour_path || '/*',
+      host: host,
       method: 'GET',
       type: 'integration',
-      fetcher: behaviour_options.name,
-      storage: behaviour_options.storage || 'local',
+      fetcher: behaviour_name,
+      storage: storage || 'local',
       timeout: 50,
-      queue: behaviour_options.queue || function (name, parameters) {
+      queue: queue || function (name, parameters) {
 
-        if (['.html', '.js', '.css', '.woff', '.woff2', '.ttf', '.eot',
-          '.svg', '.jpg', '.png', '.gif'].some(function (ext) {
+        let { path, filePath } = parameters;
+        if ([
+          '.html', '.js', '.css', '.woff',
+          '.woff2', '.ttf', '.eot', '.svg',
+          '.jpg', '.png', '.gif'
+        ].some(function (ext) {
 
-            return parameters.path.toLowerCase().endsWith(ext);
-          })) return;
-        return parameters.filePath || name;
+          return path.toLowerCase().endsWith(ext);
+        })) return;
+        return filePath || name;
       },
       parameters: {
 
@@ -62,11 +96,15 @@ module.exports = function (options) {
 
           type: 'body'
         },
+        stats: {
+
+          type: 'body'
+        },
         filename: {
 
           type: 'body'
         },
-        stats: {
+        cache: {
 
           type: 'body'
         }
@@ -85,43 +123,57 @@ module.exports = function (options) {
       return function () {
 
         var self = init.apply(this, arguments).self();
-        var index = '/' + (behaviour_options.index || 'index.html');
+        let {
+          filePath: path,
+          ranges
+        } = self.parameters;
+        var index = '/' + (default_index || 'index.html');
         var error = null;
         var ignore = false;
         var stream = null;
         var stats = null;
         var components = null;
-        var path = self.parameters.filePath;
-        var folder = typeof path !== 'string' || path.length === 0;
+        var folder = typeof path !== 'string';
+        if (!folder) folder != path.length === 0;
         if (!folder) {
 
           components = path.split('/');
-          var last = (components.splice(components.findIndex(function (component) {
+          var component_index = components.findIndex(...[
+            function (component) {
 
-            return component.indexOf('?') > -1;
-          })).shift() || components.pop()).split('?')[0];
+              return component.indexOf('?') > -1;
+            }
+          ]);
+          var last = components.splice(component_index).shift();
+          if (!last) last = components.pop();
+          last = last.split('?')[0];
           components.push(last);
           folder = last.split('.').length < 2;
         }
         if (folder) {
 
-          var root = !components || components.length < 2 || components[1] == '';
-          path = (!root ? components.join('/') : '') + index;
+          var root = !components || components.length < 2;
+          if (!root) root |= components[1] == '';
+          path = !root ? components.join('/') : '';
+          path += index;
         }
         path = self.parameters.path = path.split('?')[0];
-        self.begin('ErrorHandling', function (key, businessController, operation) {
+        self.begin(...[
+          'ErrorHandling', function (_, __, operation) {
 
-          operation.error(function (e) {
+            operation.error(...[
+              !ignore && mapError ? mapError : function (e) {
 
-            return ignore ? undefined : error || e;
-          }).apply();
-        });
-        self.begin('Fetch', function (key, businessController, operation) {
+                return ignore ? undefined : error || e;
+              }
+            ]).apply();
+          }
+        ]).begin('Fetch', function (_, __, operation) {
 
           operation.resource({
 
-            path: behaviour_options.directory + path,
-            ranges: self.parameters.ranges
+            path: directory + path,
+            ranges
           }).stream(function () {
 
             return function (resource_stream) {
@@ -133,29 +185,34 @@ module.exports = function (options) {
             if (e) error = e;
             if (resource) stats = resource.stats;
           }).apply();
-        }).use(function (key, businessController, next) {
+        }).use(function (_, __, next) {
 
-          if (error && folder && path != index) self.run(behaviour_options.name, {
+          if (error && folder && path != index) {
 
-            filePath: index
-          }, function (response, e) {
+            self.run(behaviour_name, {
 
-            ignore = !!response && !e;
-            if (response) {
+              filePath: index
+            }, function (response, e) {
 
-              stream = response.stream;
-              path = index;
-            }
-            next();
-          }); else next();
-        }).begin(function (key, businessController, operation) {
+              ignore = !!response && !e;
+              if (response) {
+
+                stream = response.stream;
+                path = index;
+              }
+              next();
+            });
+          } else next();
+        }).begin(function (_, __, operation) {
 
           operation.callback(function (response) {
 
             response.stream = stream;
-            response.filename = path.split('/').pop();
             response.stats = stats;
-            response.cache = !response.filename.toLowerCase().endsWith('.html');
+            var filename = path.split('/').pop();
+            response.filename = filename;
+            filename = filename.toLowerCase();
+            response.cache = !filename.endsWith('.html');
           }).apply();
         }).when('ModelObjectMapping');
       };
